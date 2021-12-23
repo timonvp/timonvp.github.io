713825c48daa95576f1b63639314c98b79060196
Timon Van Poucke <timonvanpoucke@hotmail.com>
2021-12-23 17:39:07 +0100

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Recipe from './components/Recipe';
-import RECIPE_DATA from './mock-data';
+import { useFetch } from './hooks/useFetch';
 
 export default function App() {
+  const { data } = useFetch('http://localhost:9000/api/recipes');
   return (
     <div className='App grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1'>
-      {RECIPE_DATA.map(v => <Recipe {...v} key={v.id} />)}
+      {data.map(v => <Recipe {...v} key={v.id} />)}
     </div>
   );
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {useState, useEffect, useCallback} from 'react';
+
+export function useFetch(uri) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  const fetchData = useCallback(async (uri) => {
+    try {
+      setError();
+      setLoading(true);
+      const {data} = await axios.get(uri);
+      setData(data.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData(uri);
+  }, [uri, fetchData]);
+
+  return {loading, data, error};
+}
