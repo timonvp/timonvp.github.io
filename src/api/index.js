d9d88ef1594e7ac88a2ca15f461c277a76640b45
Timon Van Poucke <timonvanpoucke@hotmail.com>
2022-08-19 23:45:31 +0200

    Fix deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axiosRoot from 'axios';
 import config from '../config.json';
 
-export const apiPrefix = JSON.parse(process.env.REACT_APP_API_PREFIX || '"http://localhost:9000"');
+export const apiPrefix = JSON.parse(process.env.REACT_APP_API_PREFIX || '"http://localhost:9000/api"');
 console.log(apiPrefix);
 
 export const axios = axiosRoot.create({
