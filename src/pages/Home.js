b18da174165d6aa1209c839730b43c77f41ba9e3
Timon Van Poucke <timonvanpoucke@hotmail.com>
2022-08-19 23:57:49 +0200

    Fix useFetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { useFetch } from '../hooks/useFetch';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
-    const { data } = useFetch('/api/recipes/own');
+    const { data } = useFetch('/recipes/own');
     return (<>
         <Navbar />
         <div className='App grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 pt-28' >
--- src/pages/OtherRecipes.js
+++ src/pages/OtherRecipes.js
@@ -3,7 +3,7 @@ import { useFetch } from '../hooks/useFetch';
 import Navbar from '../components/Navbar';
 
 export default function OtherRecipes() {
-    const { data } = useFetch('/api/recipes/other');
+    const { data } = useFetch('/recipes/other');
     return (<>
         <Navbar />
         <div className='App grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 pt-28' >
